1081c2b9d770f2e139ae6ed07cfd3902d8abcafd
AceMido <acemido2004@gmail.com>
2023-03-16 12:45:02 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
+
 {
     char c = 's';
     int x = 3;
@@ -6,7 +13,7 @@ int main(void)
     long long int z = 5;
     float f = 4.3;
     printf("Size of a char: %i byte(s)\n", sizeof(c));
-    printf("Size of an int%i byte(s)\n", sizeof(x));
+    printf("Size of an int: %i byte(s)\n", sizeof(x));
     printf("Size of a long: %i byte(s)\n", sizeof(y));
     printf("Size of a long long int: %i byte(s)\n", sizeof(z));
     printf("Size of a float :%i byte(s)\n", sizeof(f));
