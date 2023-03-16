cbaaa6b413298bd73eed30423173355acbfbe3db
AceMido <acemido2004@gmail.com>
2023-03-16 12:49:14 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %i byte(s)\n", sizeof(c));
 printf("Size of an int: %i byte(s)\n", sizeof(x));
 printf("Size of a long int: %i byte(s)\n", sizeof(y));
 printf("Size of a long long int: %i byte(s)\n", sizeof(z));
-printf("Size of a float :%i byte(s)\n", sizeof(f));
+printf("Size of a float: %i byte(s)\n", sizeof(f));
 return (0);
 }
