872d49ce9bb337b2c27f849cf049dd9975ab3494
AceMido <acemido2004@gmail.com>
2023-03-16 12:47:24 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-    char c = 's';
-    int x = 3;
-    long int y = 4;
-    long long int z = 5;
-    float f = 4.3;
-    printf("Size of a char: %i byte(s)\n", sizeof(c));
-    printf("Size of an int: %i byte(s)\n", sizeof(x));
-    printf("Size of a long: %i byte(s)\n", sizeof(y));
-    printf("Size of a long long int: %i byte(s)\n", sizeof(z));
-    printf("Size of a float :%i byte(s)\n", sizeof(f));
-    return (0);
+char c = 's';
+int x = 3;
+long int y = 4;
+long long int z = 5;
+float f = 4.3;
+printf("Size of a char: %i byte(s)\n", sizeof(c));
+printf("Size of an int: %i byte(s)\n", sizeof(x));
+printf("Size of a long int: %i byte(s)\n", sizeof(y));
+printf("Size of a long long int: %i byte(s)\n", sizeof(z));
+printf("Size of a float :%i byte(s)\n", sizeof(f));
+return (0);
 }
