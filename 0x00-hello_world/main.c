5e0da9e2fe728880fc8bd9c4325feffa6c39b0e6
AceMido <acemido2004@gmail.com>
2023-03-16 11:21:54 -0700

    t1

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("hello,world");
-}
+	printf("Holberton School");
+	return (0);
+}}
