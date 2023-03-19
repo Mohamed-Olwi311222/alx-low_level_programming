cc19f05897332943797f0fb1289aa14f17a316aa
AceMido <acemido2004@gmail.com>
2023-03-18 18:26:44 -0700

    t5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include<stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf(x);
+	printf("\n");
+	return (0);
+}
