0e9746dea08989deeb87b1aba454084954bb89fc
AceMido <acemido2004@gmail.com>
2023-03-20 02:23:27 -0700

    t100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print a combination of  numbers from 1 to 9
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+int num1, num2;
+
+for (num1 = 0; num1 <= 9; num1++)
+{
+	if (num1 >= num2)
+		continue;
+	putchar('0' + num1);
+	putchar('0' + num2);
+	if (num1 == 8 && num2 == 9)
+	{
+		putchar('\n');
+		return (0);
+	}
+	putchar(',');
+	putchar(' ');
+}
+return (0);
+}
