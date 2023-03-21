f772102f451931179378d545a058661297f15725
AceMido <acemido2004@gmail.com>
2023-03-21 00:38:07 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - call a function to print "_putchar"
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	putChar();
+}
+void putChar(void)
+{
+	printf("_putchar");
+}
