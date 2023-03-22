477fbb4e5498451cf0145d48ee72969d7992d7f6
AceMido <acemido2004@gmail.com>
2023-03-22 06:50:33 -0700

    t1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,5 +10,5 @@ void print_alphabet(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
-	return;
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main2.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAIN2_H
-#define MAIN2_H
-int _putchar(char c);
-void print_alphabet(void);
-#endif
