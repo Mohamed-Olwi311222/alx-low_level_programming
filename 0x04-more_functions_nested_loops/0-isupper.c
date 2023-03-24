41b70178fbb3e38a4b885356e9ed47dbc8debbe3
AceMido <acemido2004@gmail.com>
2023-03-24 01:20:31 -0700

    t0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * int _isupper - checks for uppercase letters
+ * _isupper - checks for uppercase letters that get it
+ * from main
  * @c: gets it from main
  * Return: 1 if @c is uppercase and 0 otherwise
  */
 int _isupper(int c)
 {
-	char ch =c;
+	char ch = c;
 
 	int is_upper = isupper(ch);
 
