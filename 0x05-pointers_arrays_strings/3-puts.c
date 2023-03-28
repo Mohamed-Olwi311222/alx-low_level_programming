32a147a1fb7b90dee9a38dced413b6a49eee7d80
AceMido <acemido2004@gmail.com>
2023-03-28 07:53:39 -0700

    t2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string from main
+ * @str: recive it from main
+ * Return: no return value (void)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
