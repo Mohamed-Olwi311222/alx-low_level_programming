4f58df36bfe8864d2fe8fa3d7aeb6cfaac81fb8d
AceMido <acemido2004@gmail.com>
2023-03-28 06:14:42 -0700

    t0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes input from main and change it to 98
+ * @n - takes it from main
+ * Return: no return value (void)
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
