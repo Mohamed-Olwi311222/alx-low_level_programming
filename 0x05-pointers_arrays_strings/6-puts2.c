4b8cc0ee42446b0cf164e71c4ec58053e36b7092
AceMido <acemido2004@gmail.com>
2023-03-28 13:51:13 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - print every 2 characters of a string
+ * @str: recive it from main
+ * Return: no return value (void)
+ */
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int count;
+
+	for (count = 0; count < length; count++)
+	{
+		if (count % 2 == 0 && count != 0)
+			continue;
+		else
+			_putchar(str[count]);
+	}
+	_putchar('\n');
+}
