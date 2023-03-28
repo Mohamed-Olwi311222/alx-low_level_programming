72725b3478a8134f852263f25b3e73bbf392859a
AceMido <acemido2004@gmail.com>
2023-03-28 13:59:20 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * puts_half - prints half the string from main
+ * @str: recive it from main
+ * Return: no return value (void)
+ */
+void puts_half(char *str)
+{
+	int middle = strlen(str) / 2;
+	int i;
+
+	for (i = 0; i < middle; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
