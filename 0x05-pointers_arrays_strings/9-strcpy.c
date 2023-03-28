d736cf8b6956574afa40c7e7e7f2d574281e064e
AceMido <acemido2004@gmail.com>
2023-03-28 15:26:15 -0700

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * char *_strcpy - copy string from 2nd parm to 1st
+ * @s: array from main
+ * @src: string from main
+ * Return: dest to main
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = strcpy(src);
+	return (dest);
+}
