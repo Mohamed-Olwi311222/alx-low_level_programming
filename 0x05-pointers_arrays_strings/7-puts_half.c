aae25117083c906b1511289dd8e3731e9e0efc9a
AceMido <acemido2004@gmail.com>
2023-03-28 15:15:04 -0700

    task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,12 @@ void puts_half(char *str)
 	int middle;
 	int final;
 	int i;
-	
+
 	middle = strlen(str) / 2;
-	if((strlen(str) % 2) != 0)
+	if ((strlen(str) % 2) != 0)
 		middle++;
 	final = strlen(str);
-	
+
 	for (i = middle; i < final; i++)
 		_putchar(str[i]);
 	_putchar('\n');
