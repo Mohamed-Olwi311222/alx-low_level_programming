35e16a6e472db5bcbf6242cd69022c14b8e23292
AceMido <acemido2004@gmail.com>
2023-03-29 19:37:22 -0700

    task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @dest: recive it from main
+ * @src: appends it to dest
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	dest = strncat(dest, src);
+	return (dest);
+}
