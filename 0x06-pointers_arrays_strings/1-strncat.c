0553f4409a07eb17c6d626464bd1d7ef1f25606c
AceMido <acemido2004@gmail.com>
2023-03-31 12:10:25 -0700

    t1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
  * *_strncat - concatenates two strings
  * @dest: recive it from main
  * @src: appends it to dest
+ * @n: numbers of chars
  * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
