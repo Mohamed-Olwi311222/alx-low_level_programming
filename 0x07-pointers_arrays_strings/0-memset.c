76edbfebf6250f9058af2444ab858e7669de64b4
AceMido <acemido2004@gmail.com>
2023-04-03 07:42:34 -0700

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * @s: the string that will @b  be added to
  * @b: the string that will be added to @s
  * @n: number of chars from @s that will be added
- * return: char to main (@s)
+ * Return: Always char to main (@s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
