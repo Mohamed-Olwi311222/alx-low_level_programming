d024c03d587c3d96fde7291ef0faefac9e173fd5
AceMido <acemido2004@gmail.com>
2023-04-06 04:33:00 -0700

    task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - print @s with recursion by pointer arithmetic
+ *
+ * @s: get it from main
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+	printf("\n");
+	}
+}
