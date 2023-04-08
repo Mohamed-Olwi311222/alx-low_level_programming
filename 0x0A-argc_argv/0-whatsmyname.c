70017de715fcb72e47a0d5e9f8739a5988510b70
AceMido <acemido2004@gmail.com>
2023-04-08 11:55:11 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name and prints a new line
+ * 
+ * @argc: number of arguments passed to the command line
+ * @arv: number of string args passed to the command line
+ * Return: always 0 (success)
+ */
+int main(int arg c, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
