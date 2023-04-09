b5edb2f0f20906a598eaf688261cef757094e278
AceMido <acemido2004@gmail.com>
2023-04-09 11:06:58 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - program that prints number of args passed and prints a new line
  *
@@ -8,10 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, sum = 0;
 
 	for (i = 0; i < argc; i++)
-		printf("%i", i);
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d",argc);
 	printf("\n");
 	return (0);
 }
