9d699d9f97e9da201513a59c250cdcc8c5dfef6b
AceMido <acemido2004@gmail.com>
2023-04-09 11:12:11 -0700

    task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints number of args passed and prints a new line
+ *
+ * @argv: string of arguments passed to main
+ * @argc: number of arguments passed to the command line
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul;
+
+	for (i = 1; i < argc; i++)
+	{
+		mul *= argv[i];
+		printf("%d\n", mul);
+	}
+	return (0);
+}
