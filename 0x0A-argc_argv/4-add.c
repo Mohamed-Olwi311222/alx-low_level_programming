079a60c9161ae1609cf0d66171df59d487970474
AceMido <acemido2004@gmail.com>
2023-04-09 21:09:32 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - program that prints number of args passed and prints a new line
+ *
+ * @argv: string of arguments passed to main
+ * @argc: number of arguments passed to the command line
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(atoi(argv[i])) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
