a41f68cd0e23c3d7bc40debf13b7a7f69bd5539f
AceMido <acemido2004@gmail.com>
2023-04-12 13:29:13 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,10 @@ char *str_concat(char *s1, char *s2)
 	length2 = 0;
 	sum = 0;
 	x = 0;
-
+	if (s1 == NULL)
+		s1 = " ";
+	if (S2 == NULL)
+		s2 = " ";
 	while (s1[length1] != '\0')
 		length1++;
 	while (s2[length2] != '\0')
