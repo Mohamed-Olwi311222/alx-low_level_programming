9690d475931f6b2cf8823b20edd6a657aefdccdf
AceMido <acemido2004@gmail.com>
2023-04-12 11:40:56 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
 	ptr = (char *) malloc(sizeof(char) * size);
-
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 		*(ptr + i) = c;
