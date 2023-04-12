f875fe92e9ee0ecba228d66e4eb69c631b9f1d15
AceMido <acemido2004@gmail.com>
2023-04-12 12:05:48 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strdup  - make a duplicate of @str
+ *
+ * @str: string	to dup
+ *
+ * Return: NULL if str == NULL , pointer ptr otherwise
+ **/
+char *_strdup(char *str)
+{
+	int length;
+	int i;
+	char *dup;
+
+	while (str[length] != NULL)
+		lenght++;
+	dup = (char *) malloc(sizeof(char) * lenght);
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length; i++)
+		*(dup + i) = *(str + i);
+	return (dup);
+}
