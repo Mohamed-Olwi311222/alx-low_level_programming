599053be1cef9c0bdf85a74d41efc40099857db3
AceMido <acemido2004@gmail.com>
2023-04-14 14:36:38 -0700

    task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * string_nconcat - cocatenates two strings
+ *
+ * @s1: string no1
+ * @s2: string no2
+ * @n: number of chars of @s2
+ * Return: a pointer to the new string or NULL if failed
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int length1;
+	unsigned int length2;
+	unsigned int sum;
+	unsigned int i;
+	unsigned int x;
+	char *str;
+	char tmp;
+
+	length1 = 0;
+	length2 = 0;
+	sum = 0;
+	x = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[length1] != '\0')
+		length1++;
+	while (s2[length2] !+ '\0')
+		length2++;
+	if (length2 <= n)
+		n = length2;
+	sum = length1 + n + 1;
+	str = (char *) calloc(sum, sizeof(char));
+	if (str == NULL)
+		return NULL;
+	for (i = 0; i <= length1; i++)
+	{
+		tmp = *(s1 + i);
+		*(str + i) = tmp;
+	}
+	for (i = length1; i < sum; i++)
+	{
+		if (x == n)
+			break;
+		tmp = *(s2 + x);
+		*(str + i) = tmp;
+		x++;
+	}
+	return (str);
+}
