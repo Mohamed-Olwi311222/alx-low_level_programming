a2fa5d2d85ef624e645710aa19728e05ffec292e
AceMido <acemido2004@gmail.com>
2023-04-14 14:44:29 -0700

    task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,4 @@
 #include <stdlib.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
