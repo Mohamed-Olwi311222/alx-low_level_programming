16083ff2affa25a52e9cce5765bd47cd930270d1
AceMido <acemido2004@gmail.com>
2023-04-14 15:02:24 -0700

    task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array of @nmemb elements of @size bytes each
+ *
+ * @nmemb: number of elements of an arry
+ * @size: size of elements
+ *
+ * Return: returns a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
