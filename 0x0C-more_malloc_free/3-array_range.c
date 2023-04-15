8ed34d1fff59024b73c3c9b1cf42be50d201621e
AceMido <acemido2004@gmail.com>
2023-04-14 17:04:18 -0700

    task

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = (int *) malloc(sizeof(int) * max);
+	ptr = (int *) malloc(sizeof(int) * (max + 1));
 	if (ptr == NULL)
 		return (NULL);
 	x = min;
