8d8d5724139bc82d56ca43d778e1556850bdb955
Mohamed Olwi <acemido2004@gmail.com>
2023-04-18 23:51:14 +0200

    task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - prints the filename
  *
  * Return: 0 (success)
- * */
+ */
 int main(void)
 {
-	printf("%s",__FILE__);
+	printf("%s", __FILE__);
 	return (0);
 }
