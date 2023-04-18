0e478e3fc6583f48fb222900b4c06cba1f799105
Mohamed Olwi <acemido2004@gmail.com>
2023-04-19 00:00:31 +0200

    task

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _OBJECT_LIKE_MACRO_H 
+#ifndef _OBJECT_LIKE_MACRO_H
 #define _OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) (X) < (0) ? (-(X)): (X)
+#define ABS(X) ((X) < (0)) ? (-(X)) : (X)
 #endif /* _FUNCTION_LIKE_MACRO_H */
