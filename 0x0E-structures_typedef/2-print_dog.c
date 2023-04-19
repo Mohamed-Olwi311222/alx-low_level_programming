67c797976fa95e441c3e0b2df6ea21236e663367
root <root@AceMidos-Laptop.localdomain>
2023-04-19 21:36:24 +0200

    task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,33 +7,12 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL && d->age != 0 && d->owner != NULL)
+	if (d)
 	{
-		printf("Name: (nil)\n");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
-	else if (d->age == 0 && d->name != NULL && d->owner != NULL)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: (nil)\n");
-		printf("Owner: %s\n", d->owner);
-	}
-	else if (d->owner == NULL && d->age != 0 && d->name != NULL)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: (nil)");
-	}
-	else if (d->name != NULL && d->age != 0 && d->owner != NULL)
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("OWner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 	else
 		printf("\n");
-	return;
-
-
 }
