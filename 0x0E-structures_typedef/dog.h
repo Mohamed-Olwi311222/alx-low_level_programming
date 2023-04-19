96f50724e6e782535801343a25a6b0506d1a5c2a
root <root@AceMidos-Laptop.localdomain>
2023-04-19 21:55:10 +0200

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: the dog age
  * @owner: the dog owner
  */
-struct dog
+typdef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
