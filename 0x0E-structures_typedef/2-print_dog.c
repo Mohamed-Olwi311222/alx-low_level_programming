6431c660cc04860d1d229d853c0a174dd2855a40
root <root@AceMidos-Laptop.localdomain>
2023-04-19 16:58:21 +0200

    task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,37 @@
+
+#include "dog.h"
+/**
+ * print_dog - print the description of the dog
+ * @d: the struct dog
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+	else if (d->age == NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: (nil)\n");
+		printf("Owner: %s\n", d->owner);
+	}
+	else if (d->owner == NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: (nil)");
+	}else if (d->name != NULL && d->age != NULL && d->owner != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n" d->owner);
+	}
+	else
+		printf("\n");
+
+
+}
