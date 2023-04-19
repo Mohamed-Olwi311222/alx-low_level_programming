5ecd814ef52afb4f6e56c07a588f4ba17096abd6
root <root@AceMidos-Laptop.localdomain>
2023-04-19 16:04:24 +0200

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct a dog lmao
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
