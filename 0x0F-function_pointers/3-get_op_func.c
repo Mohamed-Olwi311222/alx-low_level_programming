2a6a833761c827b3938bc7fe1979d3edc20c41e0
root <root@AceMidos-Laptop.localdomain>
2023-04-21 01:18:16 +0200

    task

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "calc.h"
+/**
+ * get_op_func - function to select the correct function of ops
+ * @s: operators
+ * Return: NULL if no op selected otherwise result
+ */
+int (*get_op_func(char *s))(int, int)
+{ 
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+    i = 0;
+    while (i != 6 )
+    {
+	    if (ops[i].op == arv[2])
+		    return (ops[i].f);
+	    i++;
+    }
+    return (NULL);
+}
