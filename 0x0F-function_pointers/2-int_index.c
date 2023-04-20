eef2a9fa1d68b12001ac24685f3305fd0edb9cda
root <root@AceMidos-Laptop.localdomain>
2023-04-20 22:11:56 +0200

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,21 @@
 #include "function_pointers.h"
 /**
- *
+ * int_index - searches for an int
+ * @array: array to search in
+ * @size: size of @array
+ * cmp: pointer to the function to be used to compare value
+ * Return: -1 if no element found other the index found
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	if (array != NULL && size != 0 && cmp != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			if (array[i] == cmp())
+				return (i);
+			else
+				return (-1);
+	}
 }
