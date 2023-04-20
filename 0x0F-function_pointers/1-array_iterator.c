8a6c4fa6b470df1b63ec1d99661ac8660f5b913d
root <root@AceMidos-Laptop.localdomain>
2023-04-20 20:41:13 +0200

    task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	{
 		size_t i;
 
-		for (i = 0; i <= size; i++)
-			action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array++);
 	}
 }
