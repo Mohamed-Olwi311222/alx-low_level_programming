3c71349df034c2c4d0dae9825f8cba6abb845f42
root <root@AceMidos-Laptop.localdomain>
2023-04-21 04:44:15 +0200

    task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[1] == "/" || ((argv[1] == "%") && (atoi(argv[3]) == 0)))
+	if (argv[2][0] == '/' || ((argv[2][0] == '%') && (atoi(argv[3]) == 0)))
 	{
 		printf("Error\n");
 		exit(100);
