f5fa46688152496bb9aafec15eeac9b25d576da2
root <root@AceMidos-Laptop.localdomain>
2023-04-22 03:18:52 +0200

    task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings separated by @separator
+ * @separator: separator of the strings
+ * @n: numbers of strings passed to it
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list nptr;
+	unsigned int i;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(nptr, n);
+
+	for(i = 1; i <= n; i++)
+	{
+		printf("%s", va_arg(n, char));
+		if (i < n)
+			printf("%c", separator);
+	}
+	printf("\n");
+	va_end(nptr);
+}
