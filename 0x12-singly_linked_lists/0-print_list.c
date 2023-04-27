ea139bb93dacc8ad9a7c5897f175887be54e1433
AceMido <acemido2004@gmail.com>
2023-04-27 21:38:08 +0200

    t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,19 @@
 #include "lists.h"
+/**
+ * _strlen - get the length of a string
+ * @s: a string to count the length of it
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int c = 0;
 
+	if (s == NULL)
+		return (0);
+	while (*s++ != NULL)
+		i++;
+	return (i);
+}
 /**
  * print_list - print a linked list
  * @h: pointer to the first node
@@ -19,7 +33,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n", strlen(h->str), h->str);
+			printf("[%d] %s\n", _strlen(h->str), h->str);
 		}
 		h = h->next;
 	}
