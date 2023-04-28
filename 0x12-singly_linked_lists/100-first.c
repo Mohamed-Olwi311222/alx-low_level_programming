af51e07ade8acdb6a3a86120903c12aac32e4085
AceMido <acemido2004@gmail.com>
2023-04-28 16:38:22 +0300

    t

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 /**
- * before_main - a constructor that is called before the main
+ * before_main - a constructor that is called before main
  */
-void __attribute__((constructor)) before_main() {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+void __attribute__((constructor)) before_main()
+{
+    printf("You're beat! and yet,");
+    printf(" you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
