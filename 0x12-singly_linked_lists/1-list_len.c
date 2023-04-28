e191d15f050abb43877b1f3f75a0a68564ae24ed
AceMido <acemido2004@gmail.com>
2023-04-28 03:58:15 +0300

    t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * list_len - gets the length of the list
+ * @h: the head of the list
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+		count++;
+	return (count);
+}
