ca3bfd0c66fb6e8a81d65cbb1a3d4a7c6c701145
AceMido <acemido2004@gmail.com>
2023-04-28 05:06:02 +0300

    t

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node_end - add node at the end
+ * @head: head of the list
+ * @str: a string to be duplicated
+ * Return: the address of the new element or NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+
+	if (!newnode || !head)
+		return (NULL);
+	if (str)
+	{
+		newnode->str = strdup(str);
+		if (newnode->str == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+	}
+	newnode->len = _strlen(newnode->str);
+	*head = newnode;
+	return (newnode);
+}
