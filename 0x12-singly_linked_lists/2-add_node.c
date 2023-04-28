c8161ccdc77ad08ff02ad8d385de376b54679bda
AceMido <acemido2004@gmail.com>
2023-04-28 04:56:29 +0300

    t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 			free(newnode);
 			return (NULL);
 		}
+		newnode->len = _strlen(newnode->str);
 		newnode->next = *head;
 		*head = newnode;
 	}
