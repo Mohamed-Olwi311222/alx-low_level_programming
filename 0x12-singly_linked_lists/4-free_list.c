bfa15b025464579ebf03bbcbd4195c4522e84e8e
AceMido <acemido2004@gmail.com>
2023-04-28 15:57:27 +0300

    t

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,15 @@
 void free_list(list_t *head)
 {
 	list_t *tmp = head;
+	list_t next;
 
 	if (!head)
 		return;
 	while (tmp != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
 	}
 	
 }
