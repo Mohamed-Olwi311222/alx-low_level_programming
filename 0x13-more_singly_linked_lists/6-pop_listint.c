18354561d639f5b53ace73306e9870fcc063567c
AceMido <acemido2004@gmail.com>
2023-04-29 20:38:08 +0300

    update

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - removes the head of the list
+ * @head: the head of the list
+ * Return: n or 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *newhead = *head;
+	int n;
+
+	if (head)
+		return (0);
+	n = (*head)->n;
+	free(*head);
+	return (n);
+}
