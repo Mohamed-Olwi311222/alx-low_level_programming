ec8cdbdb0e8fb6ca5fc43933a9c330c47ff365f4
AceMido <acemido2004@gmail.com>
2023-04-30 14:21:28 +0300

    update

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lisits.h"
+/**
+ * sum_listint - gets the sum if int
+ * @head: head of the list
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
