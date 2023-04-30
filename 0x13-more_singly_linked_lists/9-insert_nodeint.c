b82e84fb1cab6fb64affc21f308af8c34d816def
AceMido <acemido2004@gmail.com>
2023-04-30 20:10:55 +0300

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		index++;
 		if (index == idx)
 		{
-			ptr->next = ptr->next;
+			newnode->next = ptr->next;
 			ptr->next = newnode;
 		}
 	}
