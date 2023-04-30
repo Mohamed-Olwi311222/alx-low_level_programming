64ffa491e1ce43cbad75f9599424760ac75325d5
AceMido <acemido2004@gmail.com>
2023-04-30 15:03:04 +0300

    update

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get the index of the node
+ * @head: the head of the node
+ * @index: the index of the node
+ * Return: the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = malloc(sizeof(listint_t));
+	unsigned int idx = 0;
+
+	if (!node)
+		return (NULL);
+	while (idx++ == index)
+	{
+		node = head->next;
+	}
+	return (node);
+
+}
