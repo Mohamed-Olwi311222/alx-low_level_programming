35b7c8972062322fada07930cf6a3bdb1870f230
AceMido <acemido2004@gmail.com>
2023-06-08 23:42:59 +0300

    task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,35 @@
 #include "main.h"
 /**
- * tmp - the recursive function
+ * check_pallindrome - the recursive function
  * @s: string to check if pallindrome
  * @i: left index
  * @j: right index
- * Return: 1 if i > j as its now pallindrom otherwise 0
+ * Return: true if i > j as its now pallindrom otherwise false
  */
-bool tmp(char *s, int i, int j)
+bool check_pallindrome(char *s, int i, int j)
 {
 	if (i > j)
 		return (true);
 	else if (s[i] == s[j])
-		tmp(s, i + 1, j - 1);
+		return check_pallindrome(s, i + 1, j - 1);
 	else
 		return (false);
 }
 /**
- * is_palindrome - check if the string @s is pallindrome
+ * is_palindrome - call check_pallindrome to check the string
  * @s: string to check
- * Return: 1 if pallindrom 0 otherwise
+ * Return: 1 if check_pallindrom returns true, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int size = 0;
+	int size = strlen(s);
 	bool ans = false;
 
-	while (s[size] != '\0')
-		size++;
-	j = tmp(s, i, size);
-	if (j == false)
+	if (size == 0 || size == 1)
+		return (1);
+	
+	ans = check_pallindrome(s, 0, size - 1);
+	if (ans == false)
 		return (0);
 	else
 		return (1);
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
