cf4b53d67f59708a23e352ccf35a0784ba3fa89d
AceMido <acemido2004@gmail.com>
2023-06-09 23:51:14 +0300

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,6 +3,7 @@
  * main - prints the number of arguments passed into it
  * @argc: number of elements of array argv
  * @argv: array that contains command line args
+ * Return: always 0 until argv = null
  */
 int main(int argc, char *argv[])
 {
