fa970924ecc7222970e52e9e4ca5703b82ad9927
AceMido <acemido2004@gmail.com>
2023-06-09 21:58:04 +0300

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include "main.h"
+/**
+ * main - print the name of the app followed by a newline
+ * @argc: the number of elements in argv array
+ * @argv: the array that contains the command line args
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-argc]);
+	printf("%s\n", argv[argc - argc]);
 	return (0);
 }
