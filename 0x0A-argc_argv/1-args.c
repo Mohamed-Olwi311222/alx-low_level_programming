ed52f4f7ef1dad7aa51453f1aa51be147bf476b1
AceMido <acemido2004@gmail.com>
2023-06-09 23:38:31 +0300

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of elements of array argv
+ * @argv: array that contains command line args
+ */
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #endif
