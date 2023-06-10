819be24ee7f5da8f72a865cca777f65b77add162
AceMido <acemido2004@gmail.com>
2023-06-10 17:21:26 +0300

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,9 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		return (+1);
+	{
+		_putchar(43);
+		return (1);
+	}
 	if (n < 0)
+	{
+		_putchar(45);
 		return (-1);
+	}
 	else
-		return (00);
+	{
+		_putchar(48);
+		return (0);
+	}
 }
