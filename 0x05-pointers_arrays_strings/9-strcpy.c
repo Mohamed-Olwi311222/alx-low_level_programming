2374071659675d3d6027a70308b05c054b07efd2
root <root@AceMidos-Laptop.localdomain>
2023-06-10 18:20:31 +0300

    task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,10 @@ char *_strcpy(char *dest, char *src)
 {
 	char tmp;
 	int len;
+	int i;
 
 	len = _strlen(src);
-	for (int i = 0; i <= len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		tmp = src[i];
 		dest[i] = tmp;
