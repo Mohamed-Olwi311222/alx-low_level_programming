e24be32ae9904203450dce4b6a894bdbb417a38d
AceMido <acemido2004@gmail.com>
2023-06-19 17:13:34 +0300

    fixed an error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", (d->name) ? (d->name) : "(nil)");
-	if (d->age == NULL)
-		printf("Age: \n");
-	else
-		printf("Age: %d\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner) ? (d->owner) : "(nil)");
 }
