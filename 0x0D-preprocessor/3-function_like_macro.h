bf33e6275678bea1e893602991afb9faba5d8a33
AceMido <acemido2004@gmail.com>
2023-06-19 14:17:42 +0300

    task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _FUNCTION_LIKE_MACRO_H
-#define _FUNCTION_LIKE_MARCO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < (0) ? (-(x)) : (x))
 
-#endif
+#endif /* FUNCTION_LIKE_MACRO_H */
