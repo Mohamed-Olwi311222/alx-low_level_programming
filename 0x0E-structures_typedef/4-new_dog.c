6659a5b49c81fa59ebec60d04cd6278ab7e8f46c
AceMido <acemido2004@gmail.com>
2023-06-19 17:36:29 +0300

    fixed a bug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,28 +10,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = (char *) malloc(strlen(name) + 1);
+	dog->name = (char *) malloc(sizeof(char) * (strlen(name) + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->name = strcpy(dog->name, name);
 
-	dog->age = age;
 
-	dog->owner = (char *) malloc(strlen(owner) + 1);
+	dog->owner = (char *) malloc(sizeof(char) * (strlen(owner) + 1));
 	if (dog->owner)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
+	dog->name = strcpy(dog->name, name);
+	dog->age = age;
 	dog->owner = strcpy(dog->owner, owner);
-
 	return (dog);
 }
