6a93e36f4426eaab9b2b8ef5d58905ba8f945d09
AceMido <acemido2004@gmail.com>
2023-06-21 20:22:25 +0300

    test 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || !array)
+	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
