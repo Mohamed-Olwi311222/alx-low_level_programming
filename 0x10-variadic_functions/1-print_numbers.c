f6566edf7adf8f7294e43f5b8293db1b7ccb5169
AceMido <acemido2004@gmail.com>
2023-06-22 18:15:45 +0300

    test 1 with _putchar

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,20 @@
 #include "variadic_functions.h"
+/**
+ * print_numbers - print the numbers followed by a new line
+ * @separator: the separator used
+ * @n: number of parameters
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	int i;
+	va_list ptr;
+	char *s = separator;
+
+	if (s == NULL)
+		s = '';
+	if (!n)
+		return;
+	va_start(ptr,n);
+	for (i = 2; i <= n; i++)
+		_putchar((char) va_arg(ptr, n));
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,7 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
