ffe0ff10b52eb223a9fcc478cc5addc3224a7817
AceMido <acemido2004@gmail.com>
2023-07-01 02:27:39 +0300

    test 3

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,13 +6,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t count;
-	const list_t *ptr = h;
+	size_t count = 0;
 
-	while (ptr)
+	while (h)
 	{
 		count++;
-		ptr = ptr->next;
+		h = h->next;
 	}
 	printf("-> %li elements\n", count);
 	return (count);
