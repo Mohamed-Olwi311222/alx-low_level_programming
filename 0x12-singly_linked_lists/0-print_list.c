26263ac9e6e78b810e881f8d167850edeba8b247
AceMido <acemido2004@gmail.com>
2023-06-30 09:18:30 +0300

    test 1' '

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of the list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+	const list_t *ptr = h;
+
+	while (ptr)
+	{
+		printf("[%d] %s\n", strlen(ptr->str), ptr->str ? ptr->str : "(nil)");
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
