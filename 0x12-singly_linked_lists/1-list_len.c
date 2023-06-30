410f77ba74282c0fbd62a45489aa53a5f91daeb0
AceMido <acemido2004@gmail.com>
2023-07-01 02:24:28 +0300

    test 1:

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - gets the number of elements in the list
+ * @h: head of the list
+ * Return: the number of elements of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	list_t *ptr = h;
+
+	while (ptr)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	printf("-> %li elements\n", count);
+	return (count);
+}
