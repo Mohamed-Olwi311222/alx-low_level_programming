5ad1f6bcb2162253582280c03027d826f67ec4b5
AceMido <acemido2004@gmail.com>
2023-07-01 02:21:37 +0300

    test 4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 /**
+ * _strlen - get the length of the string
+ * @s: string to count the length of it
+ * ReturnL the length of the string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		return (count);
+	while (*(s++))
+		c++;
+	return (count);
+}
+/**
  * print_list - prints all elements of the list
  * @h: head of the list
  * Return: the number of nodes
@@ -11,7 +26,7 @@ size_t print_list(const list_t *h)
 
 	while (ptr)
 	{
-		printf("[%ld] %s\n", strlen(ptr->str), ptr->str ? ptr->str : "(nil)");
+		printf("[%d] %s\n", _strlen(ptr->str), ptr->str ? ptr->str : "(nil)");
 		count++;
 		ptr = ptr->next;
 	}
