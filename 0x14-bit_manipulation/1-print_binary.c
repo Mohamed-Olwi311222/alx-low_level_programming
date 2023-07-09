23fd06b3d39a0b29d6ddcade1513b9e4f176b91b
AceMido <acemido2004@gmail.com>
2023-07-09 04:34:03 +0300

    test 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
- * print_binary - converts int to binary
+ * print_binary - converts an int to binary
  * @n: the int to convert
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int bin;
-	char str[20];
+	unsigned long int active;
 
-	if(!n)
+	if (n == 0)
+	{
+		printf("0");
 		return;
-	sprintf(str, "%ld", n);
-	bin = strtoul(str, NULL, 2);
-	printf("%ld\n", bin);
-
+	}
+	active = 1;
+	while (active <= n)
+		active <<= 1;
+	active >>= 1;
+	while (active > 0)
+	{
+		if (n & active)
+			printf("1");
+		else
+			printf("0");
+		active >>= 1;
+	}
 }
