5c1b8ab8da61782eb1c5305acce04d6c6726fce2
AceMido <acemido2004@gmail.com>
2023-08-17 19:52:53 +0300

    added create_new_node in 0-print_dlistint.c file instead of createNode.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,3 +16,21 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 	return (count);
 }
+/**
+ * create_new_node - create a new node
+ * @val: the value to store inside the newnode
+ * Return: a pointer to the newnode
+ */
+dlistint_t *create_new_node(const int val)
+{
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = val;
+	newnode->next = NULL;
+	newnode->prev = NULL;
+
+	return (newnode);
+}
